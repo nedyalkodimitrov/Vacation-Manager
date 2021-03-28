994a92659db807b3b578cc2d6f90045726920f47
Ketaha <dragonrevoke@gmail.com>
2021-03-28 12:33:01 +0300

    temp

---
+++ VacantionManager2/Data/VacantionManager2DbContext.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace VacantionManager2.Data
+{
+    public class VacantionManager2DbContext : IdentityDbContext
+    {
+        public VacantionManager2DbContext(DbContextOptions<VacantionManager2DbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
---
+++ VacantionManager2/Models/Roles.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace VacantionManager2.Models
+{
+    //“CEO”, “Developer”, “Team Lead” и “Unassigned”
+    public enum RolesTypes
+    {
+        [Description("CEO")] CEO = 1,
+        [Description("Developer")] Developer = 2,
+        [Description("Team Lead")] TeamLead = 3,
+        [Description("Unnsigned")] Unnsigned = 4
+    }
+  
+    public class Roles
+    {
+        public RolesTypes Role { get; set; }
+        public string UserUsername { get; set; }
+        public virtual ICollection<User> Users {get;set;}
+    }
+}
